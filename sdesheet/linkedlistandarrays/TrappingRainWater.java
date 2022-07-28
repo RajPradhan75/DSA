a0cdc560eb39284fb4a90885660b860a1a771fab
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2022-07-28 16:44:00 +0530

    sde-sheet&advanced arrays-1

---
+++ sdesheet/linkedlistandarrays/TrappingRainWater.java
@@ -0,0 +1,44 @@
+package datastructures.src.com.sdesheet.linkedlistandarrays;
+
+public class TrappingRainWater {
+	
+	public static void main(String[] args) {
+		int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+		System.out.println(solve(height));
+	}
+
+	public static int solve(int[] height) {
+
+		int n = height.length;
+		int left = 0;
+		int right = n - 1;
+		int maxLeft = 0, maxRight = 0;
+		int units = 0;
+
+		while (left <= right) {
+
+			if (height[left] <= height[right]) {
+
+				if (height[left] > maxLeft)
+					maxLeft = height[left];
+				else
+					units += maxLeft - height[left];
+				left++;
+
+			}
+
+			else {
+				if (height[right] > maxRight)
+					maxRight = height[right];
+				else
+					units += maxRight - height[right];
+				right--;
+			}
+		}
+
+		return units;
+
+	}
+
+}
