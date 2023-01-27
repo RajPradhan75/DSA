d4f56b54315b0b4507546bc93d4e55bc87377f54
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2023-01-28 03:28:46 +0530

    arrays medium

---
+++ scaler/advanced/arrays3/RotateArray.java
@@ -0,0 +1,30 @@
+package datastructures.src.com.scaler.advanced.arrays3;
+
+import java.util.Arrays;
+
+public class RotateArray {
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+		int k = 3;
+		solve(nums, k);
+		System.out.println(Arrays.toString(nums));
+	}
+
+	public static void solve(int[] nums, int k) {
+		k = k % nums.length;
+		reverse(nums, 0, nums.length - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, nums.length - 1);
+	}
+
+	public static void reverse(int[] nums, int start, int end) {
+
+		while (start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+			start++;
+			end--;
+		}
+	}
+}
