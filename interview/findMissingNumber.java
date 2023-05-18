c23a4ce5f30207c4a240fb70864d3064d648eebc
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2023-05-19 03:45:51 +0530

    xor missing number

---
+++ interview/findMissingNumber.java
@@ -0,0 +1,23 @@
+package datastructures.src.com.interview;
+
+public class findMissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5};
+        int N = arr.length;
+        System.out.println(getMissingNo(arr, N));
+    }
+
+    public static int getMissingNo(int[] arr, int n) {
+            int xor = 0;
+
+            for(int i = 0; i<=n+1;i++){
+                xor ^= i;
+            }
+
+            for(int num : arr){
+                xor ^= num;
+            }
+
+            return xor;
+    }
+}
