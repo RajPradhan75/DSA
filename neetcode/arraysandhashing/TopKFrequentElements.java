6e845ed3eed7536793850b314a457be754493f80
raj <pradhan.raj5@gmail.com>
2024-09-30 01:00:39 +0530

    reformatting

--- neetcode/arraysandhashing/TopKFrequentElements.java
+++ neetcode/arraysandhashing/TopKFrequentElements.java
@@ -9,6 +9,65 @@ public class TopKFrequentElements {
         System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
 
+
+
+
+  /*  public class BruteForceTopKFrequent {
+
+        // Function to count the frequency of an element in the array
+        private static int countFrequency(int[] nums, int element) {
+            int count = 0;
+            for (int num : nums) {
+                if (num == element) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int[] topKFrequent(int[] nums, int k) {
+            int[] result = new int[k];
+            boolean[] visited = new boolean[nums.length]; // To mark elements that have already been picked
+
+            for (int i = 0; i < k; i++) {
+                int maxCount = 0;
+                int maxElement = nums[0];
+
+                // Find the element with the maximum frequency that hasn't been picked yet
+                for (int j = 0; j < nums.length; j++) {
+                    if (!visited[j]) {
+                        int frequency = countFrequency(nums, nums[j]);
+                        if (frequency > maxCount) {
+                            maxCount = frequency;
+                            maxElement = nums[j];
+                        }
+                    }
+                }
+
+                // Add the found element to the result and mark all instances of it as visited
+                result[i] = maxElement;
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] == maxElement) {
+                        visited[j] = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static void main(String[] args) {
+            int[] nums = {1, 1, 1, 2, 2, 3};
+            int k = 2;
+            int[] result = topKFrequent(nums, k);
+            System.out.println("Top " + k + " frequent elements: " + Arrays.toString(result));
+        }
+    }
+
+*/
+
+
+
     public static int[] topKFrequent(int[] nums, int k) {
 
         HashMap<Integer, Integer> freqMap = new HashMap<>();
--- neetcode/arraysandhashing/ValidAnagram.java
+++ neetcode/arraysandhashing/ValidAnagram.java
@@ -14,10 +14,14 @@ public class ValidAnagram {
 	static boolean isAnagram(String s, String t) {
 
 		int[] alphabet = new int[26];
-		for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-		for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+		for (int i = 0; i < s.length(); i++)
+			alphabet[s.charAt(i) - 'a']++;
+		for (int i = 0; i < t.length(); i++)
+			alphabet[t.charAt(i) - 'a']--;
 
-		for (int i : alphabet) if (i != 0) return false;
+		for (int i : alphabet)
+			if (i != 0)
+				return false;
 		
 		return true;
 	}
